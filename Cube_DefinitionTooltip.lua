8619edbb6c1c9d760a99916004d4935dfd6e8a02
Daniel <daniel.manning@connect.qut.edu.au>
2026-04-06 08:30:10 +1000

    Waste No longer doubles with output

--- Cube_DefinitionTooltip.lua
+++ Cube_DefinitionTooltip.lua
@@ -362,9 +362,11 @@ local function ShowIngredients(list, seen_unlocks, ingredients, def, amount, bp)
 	ingredient_list:Add("<Reg bg=item_default on_click={onclickreg}/>", { def = def, num = (amount or 1) })
 	-- show byproducts
 	if def.production_recipe and def.production_recipe.byproduct then 
-		for id, num in pairs(def.production_recipe.byproduct) do have_locks = ShowIngredient(ingredient_list, seen_unlocks, id, num) or have_locks end
+		for id, num in pairs(def.production_recipe.byproduct) do 
+			if ingredients[id] == nil then 
+				have_locks = ShowIngredient(ingredient_list, seen_unlocks, id, num) or have_locks end
+		end
 	end
-	
 	return have_locks
 end
 
--- data.lua
+++ data.lua
@@ -4,18 +4,20 @@ local package = ...
 package.includes = {
 	"Cube_Utilities.lua",
 	"Cube_Visuals.lua",
+	
 	"Cube_Items.lua",
 	"Cube_Fabricator.lua",
 	
 	"Cube_Farming.lua",
 	
 	"Cube_Components.lua",
-	
 	"Cube_Converter.lua",
 	"Cube_Pipes.lua",
 	"Cube_Frames.lua",
 	"Cube_Attack_Waves.lua",
+
 	"Cube_Techs.lua",
+
 	--"Cube_Land_Features.lua",
 	"Cube_Explorables.lua",
 	"Cube_Biomes.lua",
