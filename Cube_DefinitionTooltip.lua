50267a9428326e287dee2725b216cfc993a4a83a
Daniel <daniel.manning@connect.qut.edu.au>
2026-04-07 15:39:07 +1000

    Removed Alt from tooltip

--- Cube_DefinitionTooltip.lua
+++ Cube_DefinitionTooltip.lua
@@ -367,16 +367,16 @@ local function ShowIngredients(list, seen_unlocks, ingredients, def, amount, bp)
 				have_locks = ShowIngredient(ingredient_list, seen_unlocks, id, num) or have_locks end
 		end
 	end
-	if def.has_alt ~= nil and seen_unlocks[def.has_alt] ~= nil then 
-		-- has an alternative production recipe and it is seen
-		local faction = Game.GetLocalPlayerFaction()
-		if faction:IsUnlocked(def.has_alt) then 
-			local def_2 = data.items[def.has_alt]
-			ShowIngredients(list, seen_unlocks, def_2.production_recipe.ingredients, def_2, amount, bp)
-		else
-			list:Add("Text").text = "Unresearched alternative recipe"
-		end 
-	end
+	-- if def.has_alt ~= nil and seen_unlocks[def.has_alt] ~= nil then 
+	-- 	-- has an alternative production recipe and it is seen
+	-- 	local faction = Game.GetLocalPlayerFaction()
+	-- 	if faction:IsUnlocked(def.has_alt) then 
+	-- 		local def_2 = data.items[def.has_alt]
+	-- 		ShowIngredients(list, seen_unlocks, def_2.production_recipe.ingredients, def_2, amount, bp)
+	-- 	else
+	-- 		list:Add("Text").text = "Unresearched alternative recipe"
+	-- 	end 
+	-- end
 	return have_locks
 end
 
