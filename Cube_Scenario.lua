58bddb7bb315ea58f2af5da41545f23666039362
Daniel <daniel.manning@connect.qut.edu.au>
2026-09-16 18:14:56 +1000

    Blue and Empty cube visuals done

--- Cube_Scenario.lua
+++ Cube_Scenario.lua
@@ -183,7 +183,7 @@ function package:on_player_faction_spawn(faction, is_respawn)
 	transport.disconnected = false
 	transport.extra_data.name = "CUBEy"
 	transport:Place(loc.x+4,loc.y+4)
-	transport:AddItem("ic_cube_empty")
+	--transport:AddItem("ic_cube_empty")
 
 
 		--- new game plus bots 
